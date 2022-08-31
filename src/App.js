87290c72b8e3969b5ace052dc966cff7f1e3125b
Alicia Myrie <aliciamyrie@gmail.com>
2022-08-31 09:20:35 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react"
+import RestaurantCard from './components/RestaurantCards';
+import RemoveFavorite from './components/RemoveFavorite';
+import AddFavorite from './components/AddFavorite';
+import MenuBar from './components/MenuBar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <h1>Brunch'n</h1>
+      <AddFavorite />
+      <RemoveFavorite />
+      <RestaurantCard />
+      <MenuBar />
+      <Header />
+      <Footer />
       
-         <h1>Brunch'n</h1>
-      
-    </div>
+    </>
   );
 }
 
