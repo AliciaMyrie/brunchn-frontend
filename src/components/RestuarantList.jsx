1e068bc447964a6fa419a29db8966983a8b1fd67
Alicia Myrie <aliciamyrie@gmail.com>
2022-09-03 12:32:16 -0400

    added card component

--- src/components/RestuarantList.jsx
+++ src/components/RestuarantList.jsx
@@ -1,25 +1,49 @@
-import React from "react";
-import RestaurantCard from "./RestaurantCards"
-import { useState } from "react"
-import { useEffect } from "react";
+import React from 'react';
+import RestaurantCard from './RestaurantCards';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Card, Col, Row } from 'antd';
 
-const [RestaurantList, setRestaurantList] = useState()
+
+// const [RestaurantList, setRestaurantList] = useState()
 
 export default function RestaurantList() {
-    
-    useEffect(()=> {
-        fetch("http://localhost:4050/getrestaurants")
-        .then(results => results.json())
-        .then(Restaurants => setRestaurants(Restaurants))
-        .catch(console.error)
-    }, [setRestaurantList])
-    if (!RestaurantList){
-        return<h2>No restaurants to display</h2>
-    }
-    return (
-        <ul>
-            {}
-        </ul>
-    )
-    
+  // const [ RestaurantsList, setRestaurantsList ] = useState()
+  const [restaurants, setRestaurants] = useState();
+
+  console.log(restaurants);
+
+  useEffect(() => {
+    fetch('http://localhost:4050/getRestaurants')
+      .then((results) => results.json())
+      .then((data) => setRestaurants(data))
+      .catch(console.error);
+  }, [setRestaurants]);
+
+  return (
+    <section>
+      <div>
+        {!restaurants ? (
+          <h2>Loading .. </h2>
+        ) : 
+        <div className="site-card-wrapper">
+        <Row gutter={16}>
+          {restaurants.map((restaurants) => 
+            
+            
+            <Col span={8} key={restaurants._id}>
+            <Card title={restaurants.name} bordered={false}>
+            {restaurants.address}
+            </Card>
+          </Col>
+
+          
+
+)}
+            </Row>
+            </div>
+}       
+      </div>
+    </section>
+  );
 }
